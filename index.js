1d882ddd6fe90382c805cbd853cc1c112b346520
dipeshpatel02 <dipeshpatel827@gmail.com>
2023-04-17 16:26:10 +0530

    backend

--- index.js
+++ index.js
@@ -23,6 +23,40 @@ app.get("/api/get",(req,res)=>{
     });
 });
 
+app.post("/api/post",(req,res)=>{
+    const {name,payperday,totaldays}=req.body;
+    const sqlinsert="INSERT INTO contact(Name, PayPerDay, TotalDays) VALUES (?,?,?)";
+    db.query(sqlinsert,[name,payperday,totaldays],(error,result)=>{
+        if(error){
+            console.log(error);
+        }
+    })
+});
+
+app.put("/api/addday/:Id",(req,res)=>{
+    const {Id}=req.params;
+    const {TotalDays}=req.body;
+    const sqladdday ="UPDATE contact set TotalDays=TotalDays+1 where Id=?;";
+    db.query(sqladdday,Id,(error,result)=>{
+        if(error){
+            console.log(error);
+        }
+    })
+});
+
+app.put("/api/addhalfday/:Id",(req,res)=>{
+    const {Id}=req.params;
+    const {TotalDays}=req.body;
+    const sqladdhalfday ="UPDATE contact set TotalDays=TotalDays+0.5 where Id=?";
+    db.query(sqladdhalfday,Id,(error,result)=>{
+        if(error){
+            console.log(error);
+        }
+    })
+});
+
+
+
 app.get("/",(req,res)=>{
     // const sqlinsert="INSERT INTO contact(Name, address, price) VALUES ('yoges','nandurbar',1000)";
     // db.query(sqlinsert,(error,result)=>{
